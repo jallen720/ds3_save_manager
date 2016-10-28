c58d63b320465ba8383ed08e080910f5f3048370
Joel Allen <joelallen720@gmail.com>
2016-10-27 19:03:35 -0700

    Implement ds3 save manager.

---
+++ Main.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace DS3_Save_Manager
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backup_directory_textbox = new System.Windows.Forms.TextBox();
+            this.backup_directory_label = new System.Windows.Forms.Label();
+            this.backup_directory_browse_button = new System.Windows.Forms.Button();
+            this.backup_button = new System.Windows.Forms.Button();
+            this.revert_button = new System.Windows.Forms.Button();
+            this.backup_name_textbox = new System.Windows.Forms.TextBox();
+            this.backup_name_label = new System.Windows.Forms.Label();
+            this.backup_name_dropdown = new System.Windows.Forms.ComboBox();
+            this.revert_name_label = new System.Windows.Forms.Label();
+            this.create_temp_backup_toggle = new System.Windows.Forms.CheckBox();
+            this.backup_directory_browser = new System.Windows.Forms.FolderBrowserDialog();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backup_directory_textbox
+            // 
+            this.backup_directory_textbox.Location = new System.Drawing.Point(139, 16);
+            this.backup_directory_textbox.Name = "backup_directory_textbox";
+            this.backup_directory_textbox.ReadOnly = true;
+            this.backup_directory_textbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.backup_directory_textbox.Size = new System.Drawing.Size(576, 22);
+            this.backup_directory_textbox.TabIndex = 1;
+            this.backup_directory_textbox.TextChanged += new System.EventHandler(this.backup_directory_textbox_TextChanged);
+            // 
+            // backup_directory_label
+            // 
+            this.backup_directory_label.AutoSize = true;
+            this.backup_directory_label.Location = new System.Drawing.Point(17, 19);
+            this.backup_directory_label.Name = "backup_directory_label";
+            this.backup_directory_label.Size = new System.Drawing.Size(116, 17);
+            this.backup_directory_label.TabIndex = 2;
+            this.backup_directory_label.Text = "Backup Directory";
+            // 
+            // backup_directory_browse_button
+            // 
+            this.backup_directory_browse_button.Location = new System.Drawing.Point(721, 12);
+            this.backup_directory_browse_button.Name = "backup_directory_browse_button";
+            this.backup_directory_browse_button.Size = new System.Drawing.Size(91, 30);
+            this.backup_directory_browse_button.TabIndex = 3;
+            this.backup_directory_browse_button.Text = "Browse";
+            this.backup_directory_browse_button.UseVisualStyleBackColor = true;
+            this.backup_directory_browse_button.Click += new System.EventHandler(this.backup_directory_browse_button_Click);
+            // 
+            // backup_button
+            // 
+            this.backup_button.Location = new System.Drawing.Point(20, 131);
+            this.backup_button.Name = "backup_button";
+            this.backup_button.Size = new System.Drawing.Size(792, 30);
+            this.backup_button.TabIndex = 5;
+            this.backup_button.Text = "Backup current save as:";
+            this.backup_button.UseVisualStyleBackColor = true;
+            this.backup_button.Click += new System.EventHandler(this.backup_button_Click);
+            // 
+            // revert_button
+            // 
+            this.revert_button.Location = new System.Drawing.Point(20, 271);
+            this.revert_button.Name = "revert_button";
+            this.revert_button.Size = new System.Drawing.Size(792, 30);
+            this.revert_button.TabIndex = 6;
+            this.revert_button.Text = "Revert current save to backup:";
+            this.revert_button.UseVisualStyleBackColor = true;
+            this.revert_button.Click += new System.EventHandler(this.revert_button_Click);
+            // 
+            // backup_name_textbox
+            // 
+            this.backup_name_textbox.Location = new System.Drawing.Point(119, 103);
+            this.backup_name_textbox.Name = "backup_name_textbox";
+            this.backup_name_textbox.Size = new System.Drawing.Size(693, 22);
+            this.backup_name_textbox.TabIndex = 0;
+            this.backup_name_textbox.TextChanged += new System.EventHandler(this.backup_name_textbox_TextChanged);
+            // 
+            // backup_name_label
+            // 
+            this.backup_name_label.AutoSize = true;
+            this.backup_name_label.Location = new System.Drawing.Point(17, 106);
+            this.backup_name_label.Name = "backup_name_label";
+            this.backup_name_label.Size = new System.Drawing.Size(96, 17);
+            this.backup_name_label.TabIndex = 8;
+            this.backup_name_label.Text = "Backup Name";
+            // 
+            // backup_name_dropdown
+            // 
+            this.backup_name_dropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.backup_name_dropdown.FormattingEnabled = true;
+            this.backup_name_dropdown.Location = new System.Drawing.Point(119, 214);
+            this.backup_name_dropdown.Name = "backup_name_dropdown";
+            this.backup_name_dropdown.Size = new System.Drawing.Size(693, 24);
+            this.backup_name_dropdown.TabIndex = 9;
+            this.backup_name_dropdown.SelectedIndexChanged += new System.EventHandler(this.backup_name_dropdown_SelectedIndexChanged);
+            // 
+            // revert_name_label
+            // 
+            this.revert_name_label.AutoSize = true;
+            this.revert_name_label.Location = new System.Drawing.Point(17, 217);
+            this.revert_name_label.Name = "revert_name_label";
+            this.revert_name_label.Size = new System.Drawing.Size(96, 17);
+            this.revert_name_label.TabIndex = 10;
+            this.revert_name_label.Text = "Backup Name";
+            // 
+            // create_temp_backup_toggle
+            // 
+            this.create_temp_backup_toggle.AutoSize = true;
+            this.create_temp_backup_toggle.Checked = true;
+            this.create_temp_backup_toggle.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.create_temp_backup_toggle.Location = new System.Drawing.Point(23, 244);
+            this.create_temp_backup_toggle.Name = "create_temp_backup_toggle";
+            this.create_temp_backup_toggle.Size = new System.Drawing.Size(289, 21);
+            this.create_temp_backup_toggle.TabIndex = 12;
+            this.create_temp_backup_toggle.Text = "Create temporary backup of current save\r\n";
+            this.create_temp_backup_toggle.UseVisualStyleBackColor = true;
+            this.create_temp_backup_toggle.Click += new System.EventHandler(this.create_temp_backup_toggle_Click);
+            // 
+            // delete_button
+            // 
+            this.delete_button.Location = new System.Drawing.Point(20, 307);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(792, 30);
+            this.delete_button.TabIndex = 13;
+            this.delete_button.Text = "Delete backup:";
+            this.delete_button.UseVisualStyleBackColor = true;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 356);
+            this.Controls.Add(this.delete_button);
+            this.Controls.Add(this.create_temp_backup_toggle);
+            this.Controls.Add(this.revert_name_label);
+            this.Controls.Add(this.backup_name_dropdown);
+            this.Controls.Add(this.backup_name_label);
+            this.Controls.Add(this.backup_name_textbox);
+            this.Controls.Add(this.revert_button);
+            this.Controls.Add(this.backup_button);
+            this.Controls.Add(this.backup_directory_browse_button);
+            this.Controls.Add(this.backup_directory_label);
+            this.Controls.Add(this.backup_directory_textbox);
+            this.Name = "Main";
+            this.Text = "Dark Souls 3 Save Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox backup_directory_textbox;
+        private System.Windows.Forms.Label backup_directory_label;
+        private System.Windows.Forms.Button backup_directory_browse_button;
+        private System.Windows.Forms.Button backup_button;
+        private System.Windows.Forms.Button revert_button;
+        private System.Windows.Forms.TextBox backup_name_textbox;
+        private System.Windows.Forms.Label backup_name_label;
+        private System.Windows.Forms.ComboBox backup_name_dropdown;
+        private System.Windows.Forms.Label revert_name_label;
+        private System.Windows.Forms.CheckBox create_temp_backup_toggle;
+        private System.Windows.Forms.FolderBrowserDialog backup_directory_browser;
+        private System.Windows.Forms.Button delete_button;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DS3_Save_Manager
+{
+    public partial class Main : Form
+    {
+        private const string REGISTRY_KEY_NAME = "ds3_save_manager";
+        private const string TEMP_BACKUP_NAME = "ds3_save_manager_temp_backup";
+        private readonly string DS3_SAVE_DIRECTORY_PATH;
+        private readonly RegistryKey registry_key;
+
+        public Main()
+        {
+            InitializeComponent();
+            update_backup_button();
+            update_revert_button();
+            DS3_SAVE_DIRECTORY_PATH = Path.Combine(Environment.GetEnvironmentVariable("appdata"), "DarkSoulsIII");
+
+
+            // Retrieve registry key for application, or create it if it hasn't already been created.
+            RegistryKey software_key = Registry
+                .CurrentUser
+                .OpenSubKey("Software", true);
+
+            if ((registry_key = software_key.OpenSubKey(REGISTRY_KEY_NAME, true)) == null)
+            {
+                software_key.CreateSubKey(REGISTRY_KEY_NAME);
+                registry_key = software_key.OpenSubKey(REGISTRY_KEY_NAME, true);
+            }
+
+
+            // Load config from registry.
+            backup_directory_textbox.Text = (string)registry_key.GetValue("backup_directory");
+            create_temp_backup_toggle.Checked = bool.Parse((string)registry_key.GetValue("create_temp_backup") ?? "true");
+        }
+
+
+        #region Events
+        private void backup_directory_browse_button_Click(object sender, EventArgs e)
+        {
+            if (backup_directory_browser.ShowDialog() == DialogResult.OK)
+            {
+                backup_directory_textbox.Text = backup_directory_browser.SelectedPath;
+            }
+        }
+
+        private void backup_button_Click(object sender, EventArgs e)
+        {
+            copy_directory(DS3_SAVE_DIRECTORY_PATH, get_new_backup_path());
+            backup_name_textbox.Text = "";
+            update_backup_name_dropdown();
+        }
+
+        private void backup_directory_textbox_TextChanged(object sender, EventArgs e)
+        {
+            update_backup_name_dropdown();
+
+
+            // Update backup and revert buttons to reflect changes to backup directory.
+            update_backup_button();
+            update_revert_button();
+
+
+            // Update config with new backup directory.
+            registry_key.SetValue("backup_directory", backup_directory_textbox.Text);
+        }
+
+        private void backup_name_textbox_TextChanged(object sender, EventArgs e)
+        {
+            update_backup_button();
+        }
+
+        private void create_temp_backup_toggle_Click(object sender, EventArgs e)
+        {
+            // Update config with new create temp backup flag.
+            registry_key.SetValue("create_temp_backup", create_temp_backup_toggle.Checked);
+        }
+
+        private void revert_button_Click(object sender, EventArgs e)
+        {
+            if (create_temp_backup_toggle.Checked)
+            {
+                string temp_backup_path = get_temp_backup_path();
+
+                if (Directory.Exists(temp_backup_path))
+                {
+                    Directory.Delete(temp_backup_path, true);
+                }
+
+                copy_directory(DS3_SAVE_DIRECTORY_PATH, temp_backup_path);
+            }
+
+            Directory.Delete(DS3_SAVE_DIRECTORY_PATH, true);
+            copy_directory(get_selected_backup_path(), DS3_SAVE_DIRECTORY_PATH);
+
+            MessageBox.Show(
+                "Successfully reverted current save to backup: " + (string)backup_name_dropdown.SelectedItem,
+                "Success",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            if (create_temp_backup_toggle.Checked)
+            {
+                update_backup_name_dropdown();
+            }
+        }
+
+        private void backup_name_dropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var backup_name = (string)backup_name_dropdown.SelectedItem;
+            revert_button.Text = "Revert current save to backup: " + backup_name;
+            delete_button.Text = "Delete backup: " + backup_name;
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to permanently delete backup: " + (string)backup_name_dropdown.SelectedItem,
+                "Warning",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                Directory.Delete(get_selected_backup_path(), true);
+                update_backup_name_dropdown();
+            }
+        }
+        #endregion Events
+
+
+        #region Utilities
+        private bool is_ds3_backup_directory(string directory)
+        {
+            // Best way I can think of to verify a backup directory actually contains DS3 save data.
+            return Directory.GetFiles(directory).Contains(Path.Combine(directory, "GraphicsConfig.xml"));
+        }
+
+        private void update_backup_button()
+        {
+            backup_button.Enabled =
+                backup_directory_textbox.Text != "" &&    // Backup directory must exist
+                backup_name_textbox.Text != "" &&         // Backup name must exist
+                !Directory.Exists(get_new_backup_path()); // Backup must not already exist
+
+            backup_button.Text = "Backup current save as: " + backup_name_textbox.Text;
+        }
+
+        private void update_revert_button()
+        {
+            revert_button.Enabled = backup_directory_textbox.Text != "" && is_backup_selected();
+        }
+
+        private bool is_backup_selected()
+        {
+            return backup_name_dropdown.Items.Count > 0;
+        }
+
+        private void copy_directory(string source_directory_name, string destination_directory_name)
+        {
+            // Get the subdirectories for the specified directory.
+            var source_directory = new DirectoryInfo(source_directory_name);
+
+            if (!source_directory.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + source_directory_name);
+            }
+
+            DirectoryInfo[] dirs = source_directory.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destination_directory_name))
+            {
+                Directory.CreateDirectory(destination_directory_name);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = source_directory.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                file.CopyTo(Path.Combine(destination_directory_name, file.Name), false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                copy_directory(subdir.FullName, Path.Combine(destination_directory_name, subdir.Name));
+            }
+        }
+
+        private string get_new_backup_path()
+        {
+            return Path.Combine(backup_directory_textbox.Text, backup_name_textbox.Text);
+        }
+
+        private string get_temp_backup_path()
+        {
+            return Path.Combine(backup_directory_textbox.Text, TEMP_BACKUP_NAME);
+        }
+
+        private string get_selected_backup_path()
+        {
+            return Path.Combine(backup_directory_textbox.Text, (string)backup_name_dropdown.SelectedItem);
+        }
+
+        private void update_backup_name_dropdown()
+        {
+            backup_name_dropdown.Items.Clear();
+
+            IEnumerable<string> backup_directories = Directory
+                .GetDirectories(backup_directory_textbox.Text)
+                .Where(is_ds3_backup_directory)
+                .Select(backup_directory => backup_directory.Replace(backup_directory_textbox.Text + "\\", ""));
+
+            foreach (string backup_directory in backup_directories)
+            {
+                backup_name_dropdown.Items.Add(backup_directory);
+            }
+
+            if (is_backup_selected())
+            {
+                backup_name_dropdown.SelectedIndex = 0;
+            }
+        }
+        #endregion Utilities
+    }
+}
